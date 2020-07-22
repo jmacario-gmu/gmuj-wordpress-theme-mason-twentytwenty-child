3177cc299f6bb1e1b8f5e3140a4a3c5d3e3162b5
Jan Macario <jmacario@GMU.EDU>
2020-07-22 17:24:10 -0400

    Deregisters the 'desktop expanded' menu location added by the parent theme.

--- php/custom-menus.php
+++ php/custom-menus.php
@@ -3,6 +3,21 @@
  * php file which handles custom menus specific to this child theme.
  */
 
+
+/**
+ * Unregisters parent theme menus.
+ * We use a priority of 11 to make sure this function fires after the parent theme function adding these menus has already fired. It is at a default priority of 10.
+ */
+add_action('init', 'gmuj_deregister_parent_theme_menus',11);
+function gmuj_deregister_parent_theme_menus(){
+
+    // Deregister parent theme custom menu locations
+        // Desktop expanded menu
+        unregister_nav_menu('expanded');
+
+}
+
+
 /**
  * Registers child theme custom menus
  */
