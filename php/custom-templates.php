2ba61660ac1638d4107fae7f68cf1dba5a440ffc
Jan Macario <jmacario@GMU.EDU>
2020-07-22 19:09:10 -0400

    Removes parent theme cover template option from posts editor.

--- php/custom-templates.php
+++ php/custom-templates.php
@@ -4,10 +4,13 @@
  */
 
 /**
- * Remove unwanted page template options
+ * Remove unwanted page and post template options
+ *
+ * We have to use two filters - for both pages and posts - as WordPress treats these differently. Reference: https://wordpress.org/support/topic/remove-page-templates-in-child-theme/
  */
-add_filter('theme_page_templates','gmuj_remove_page_templates');
-function gmuj_remove_page_templates($templates) {
+add_filter('theme_page_templates','gmuj_remove_parent_theme_templates');
+add_filter('theme_post_templates','gmuj_remove_parent_theme_templates');
+function gmuj_remove_parent_theme_templates($templates) {
     
     // Remove cover template option
     unset( $templates['templates/template-cover.php'] );
