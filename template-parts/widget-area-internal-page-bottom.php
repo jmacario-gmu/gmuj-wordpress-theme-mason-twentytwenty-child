f62edef9ac02c24088a6c5e385a246346a2a0938
Jan Macario <jmacario@GMU.EDU>
2021-01-06 13:44:46 -0500

    Ensures that internal page top and bottom widget areas do not appear if they contain no widgets.

--- template-parts/widget-area-internal-page-bottom.php
+++ template-parts/widget-area-internal-page-bottom.php
@@ -1,4 +1,6 @@
 <!-- Child theme template part for the internal page bottom widget area -->
+<?php if (is_active_sidebar('sidebar-internal-page-bottom')) { ?>
 <ul class="sidebar sidebar-internal-page" id="sidebar-internal-page-bottom">
 <?php dynamic_sidebar('sidebar-internal-page-bottom'); ?>
 </ul><!-- #sidebar-internal-page-bottom -->
+<?php } ?>
--- template-parts/widget-area-internal-page-top.php
+++ template-parts/widget-area-internal-page-top.php
@@ -1,4 +1,6 @@
 <!-- Child theme template part for the internal page top widget area -->
+<?php if (is_active_sidebar('sidebar-internal-page-top')) { ?>
 <ul class="sidebar sidebar-internal-page" id="sidebar-internal-page-top">
 <?php dynamic_sidebar('sidebar-internal-page-top'); ?>
 </ul><!-- #sidebar-internal-page-top -->
+<?php } ?>
