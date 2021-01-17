2fe4d2f2af89671dd6fcccdead7793e378e7b54d
Jan Macario <jmacario@GMU.EDU>
2021-01-16 19:12:07 -0500

    Adds template handling and styles for the person custom post type.

--- template-parts/content.php
+++ template-parts/content.php
@@ -55,7 +55,14 @@
 				if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
 					the_excerpt();
 				} else {
-					the_content( __( 'Continue reading', 'twentytwenty' ) );
+
+					// If this post is a post of the custom post type: person, insert the person content template. If not, use the normal template.
+					if (get_post_type()=='person') {
+						get_template_part('template-parts/sub-content','person');
+					} else {
+						the_content( __( 'Continue reading', 'twentytwenty' ) );
+					}
+
 				}
 				?>
 
