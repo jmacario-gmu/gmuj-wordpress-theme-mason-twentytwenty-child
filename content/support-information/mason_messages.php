cdac4453ac6f7367c87c2e5a41b5e82a86e30a5f
Jan Macario <jmacario@GMU.EDU>
2022-03-04 12:48:28 -0500

    In Mason Messages feature, styles output messages.

--- content/support-information/mason_messages.php
+++ content/support-information/mason_messages.php
@@ -6,18 +6,18 @@ $mason_mesages = array();
 
 // Check whether search engines can index this website
 if (!get_option('blog_public')) {
-	array_push($mason_mesages,"Search engines are discouraged from indexing this website!");
+	array_push($mason_mesages,"<p class='notice notice-warning'>Search engines are discouraged from indexing this website!</p>");
 }
 
 // Check what the admin email is set to
 if (get_bloginfo('admin_email')!='gmuweb@gmu.edu' && get_bloginfo('admin_email')!='webmaster@gmu.edu') {
-	array_push($mason_mesages,"The website administrator email is not set to the Mason webmaster email address (webmaster@gmu.edu)!");
+	array_push($mason_mesages,"<p class='notice notice-error'>The website administrator email is not set to the Mason webmaster email address (webmaster@gmu.edu)!</p>");
 }
 
 // Display messages
 //Check whether there are any messages
 if (empty($mason_mesages)) {
-	echo "<p>No messages.</p>";
+	echo "<p class='notice notice-success'>No messages.</p>";
 } else {
 	foreach ($mason_mesages as $mason_message) {
 		echo $mason_message;
