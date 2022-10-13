8b530c1a8d75a19c1f4f4b11d446c011df768d9c
ryanlebreton-gmu <rlebreto@gmu.edu>
2022-10-13 12:57:40 -0400

    Add menu location to page footer.

--- footer.php
+++ footer.php
@@ -11,6 +11,7 @@
 			<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
 				<div class="section-inner">
+					
 
 					<div class="footer-credits">
 
@@ -28,81 +29,85 @@
 							</a>
 						</p>
 
-						<p class="footer-slogan-address-copyright-contact">
+						<div>
+							<!-- Footer Social menu -->
+							<?php get_template_part('template-parts/menu','footer-social'); ?>
 
-							<?php
-							// Display site tagline if provided in theme customizer
-							if (get_theme_mod('gmuj_site_tagline')) {
-								echo '<!--site tagline-->';
-								echo '<span class="site-slogan">'.get_theme_mod('gmuj_site_tagline').'</span>';
-								echo '<br />';
-							}
+							<p class="footer-slogan-address-copyright-contact">
 
-							// If a custom address was *not* provided in theme customizer, use default address
-							if (!get_theme_mod('gmuj_contact_address_line_1') && !get_theme_mod('gmuj_contact_address_line_2') && !get_theme_mod('gmuj_contact_address_line_3')) {
-								echo '4400 University Drive, Fairfax, Virginia 22030<br />';
+								<?php
+								// Display site tagline if provided in theme customizer
+								if (get_theme_mod('gmuj_site_tagline')) {
+									echo '<!--site tagline-->';
+									echo '<span class="site-slogan">'.get_theme_mod('gmuj_site_tagline').'</span>';
+									echo '<br />';
+								}
+								// If a custom address was *not* provided in theme customizer, use default address
+								if (!get_theme_mod('gmuj_contact_address_line_1') && !get_theme_mod('gmuj_contact_address_line_2') && !get_theme_mod('gmuj_contact_address_line_3')) {
+									echo '4400 University Drive, Fairfax, Virginia 22030<br />';
 
-							}
-							?>
+								}
+								?>
 
-							<!--Address line 1 from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_address_line_1')) {
-								echo get_theme_mod('gmuj_contact_address_line_1');
-								echo '<br />';
-							}
-							?>
+								<!--Address line 1 from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_address_line_1')) {
+									echo get_theme_mod('gmuj_contact_address_line_1');
+									echo '<br />';
+								}
+								?>
 
-							<!--Address line 2 from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_address_line_2')) {
-								echo get_theme_mod('gmuj_contact_address_line_2');
-								echo '<br />';
-							}
-							?>
+								<!--Address line 2 from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_address_line_2')) {
+									echo get_theme_mod('gmuj_contact_address_line_2');
+									echo '<br />';
+								}
+								?>
 
-							<!--Address line 3 from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_address_line_3')) {
-								echo get_theme_mod('gmuj_contact_address_line_3');
-								echo '<br />';
-							}
-							?>
+								<!--Address line 3 from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_address_line_3')) {
+									echo get_theme_mod('gmuj_contact_address_line_3');
+									echo '<br />';
+								}
+								?>
 
-							<!--Contact email address from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_email')) {
-								echo 'Email: ';
-								echo '<a href="mailto:'.get_theme_mod('gmuj_contact_email').'">'.get_theme_mod('gmuj_contact_email').'</a>';
-								echo '<br />';
-							}
-							?>
+								<!--Contact email address from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_email')) {
+									echo 'Email: ';
+									echo '<a href="mailto:'.get_theme_mod('gmuj_contact_email').'">'.get_theme_mod('gmuj_contact_email').'</a>';
+									echo '<br />';
+								}
+								?>
 
-							<!--Contact phone number from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_phone')) {
-								echo 'Call: ';
-								echo get_theme_mod('gmuj_contact_phone');
-								echo '<br />';
-							}
-							?>
+								<!--Contact phone number from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_phone')) {
+									echo 'Call: ';
+									echo get_theme_mod('gmuj_contact_phone');
+									echo '<br />';
+								}
+								?>
 
-							<!--Contact fax number from theme customizer-->
-							<?php if (get_theme_mod('gmuj_contact_fax')) {
-								echo 'Fax: ';
-								echo get_theme_mod('gmuj_contact_fax');
-								echo '<br />';
-							}
-							?>
+								<!--Contact fax number from theme customizer-->
+								<?php if (get_theme_mod('gmuj_contact_fax')) {
+									echo 'Fax: ';
+									echo get_theme_mod('gmuj_contact_fax');
+									echo '<br />';
+								}
+								?>
 
-							<!--Copyright notice-->
-							&copy; <span style="font-weight:normal;"><?php echo date("Y"); ?> George Mason University</span>
-						</p>
-
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://secure.php.net/date */
-								_x( 'Y', 'copyright date format', 'twentytwenty' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
+								<!--Copyright notice-->
+								&copy; <span style="font-weight:normal;"><?php echo date("Y"); ?> George Mason University</span>
+							</p>
+						
+							<p class="footer-copyright">&copy;
+								<?php
+								echo date_i18n(
+									/* translators: Copyright date format, see https://secure.php.net/date */
+									_x( 'Y', 'copyright date format', 'twentytwenty' )
+								);
+								?>
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+							</p><!-- .footer-copyright -->
+						</div>
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
