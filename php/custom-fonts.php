d4d7cd9a293b63166a0568275661574f9798793d
Jan Macario <jmacario@gmu.edu>
2024-04-25 16:54:32 -0400

    Per new brand standards, replaces font Roboto Slab with Figtree.

--- php/custom-fonts.php
+++ php/custom-fonts.php
@@ -4,8 +4,7 @@
 // Implement custom google fonts
 add_action( 'wp_enqueue_scripts', 'gmuj_theme_custom_fonts' );
 function gmuj_theme_custom_fonts() {
-  //wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Roboto+Slab:400,700|Roboto:400,400i,500,700' );
-  wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700|Roboto+Slab:400,300,700' );
+  wp_enqueue_style( 'fonts', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700|Figtree:300,600,700,300italic,600italic,700italic' );
 }
 
 
