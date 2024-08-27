cc287bb6ff6d110e2fc8ebce1574f6397da66623
Jan Macario <jmacario@gmu.edu>
2024-08-27 13:42:34 -0400

    Cleans-up custom sitemap code.

--- php/custom-sitemap.php
+++ php/custom-sitemap.php
@@ -4,13 +4,13 @@
  */
 
 
-/**
- * Summary: Disables users sitemap
- */
-function gmuj_disable_sitemap_users($provider, $name) {
-	return ($name == 'users') ? false : $provider;
-}
+// disable users sitemap
 add_filter('wp_sitemaps_add_provider', 'gmuj_disable_sitemap_users', 10, 2);
+function gmuj_disable_sitemap_users($provider, $name) {
+
+	return ($name == 'users') ? false : $provider;
+
+}
 
 // remove mailpoet pages from sitemap
 add_filter( 'wp_sitemaps_post_types', 'gmuw_sitemap_remove_mailpoet' );
