af5726d65e7f52d916c67d5831486e11038c3a03
Jan Macario <jmacario@gmu.edu>
2024-08-27 13:40:20 -0400

    Removes MailPoet plugin pages from WordPress sitemap system.

--- php/custom-sitemap.php
+++ php/custom-sitemap.php
@@ -11,3 +11,13 @@ function gmuj_disable_sitemap_users($provider, $name) {
 	return ($name == 'users') ? false : $provider;
 }
 add_filter('wp_sitemaps_add_provider', 'gmuj_disable_sitemap_users', 10, 2);
+
+// remove mailpoet pages from sitemap
+add_filter( 'wp_sitemaps_post_types', 'gmuw_sitemap_remove_mailpoet' );
+function gmuw_sitemap_remove_mailpoet($post_types) {
+
+	unset($post_types['mailpoet_page']);
+
+	return $post_types;
+
+}
