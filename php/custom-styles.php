ecd6be2a4d3d20bda3ed6baf043d27735ca67bd2
Jan Macario <jmacario@gmu.edu>
2024-09-05 14:40:19 -0400

    Consolidates stylesheets into fewer total stylesheets and enqueues them individually. Ensures that stylesheets are enqueued in such a way as to avoid browser caching in order to prepare for new theme version rollout which includes major branding updates.

--- php/custom-styles.php
+++ php/custom-styles.php
@@ -9,22 +9,148 @@
 add_action( 'wp_enqueue_scripts', 'gmuj_enqueue_styles' );
 function gmuj_enqueue_styles() {
  
- 	// Name the parent theme stylesheet
-    $parent_style = 'twentytwenty-style';
- 
- 	// Enqueue the parent theme stylesheet
+    //enqueue fontawesome
     wp_enqueue_style(
-    	$parent_style, //stylesheet name
-    	get_template_directory_uri() . '/style.css' //path to parent (template) theme stylesheet
+        'gmuw-style-fontawesome',
+        get_stylesheet_directory_uri() . '/css/fontawesome-all.min.css',
+    );
+
+	// Enqueue the parent theme stylesheet
+    wp_enqueue_style(
+        'twentytwenty-style',
+        get_template_directory_uri() . '/style.css'
     );
 
     // Enqueue the general child theme stylesheet
     wp_enqueue_style(
-    	'child-style', //stylesheet name
-        get_stylesheet_directory_uri() . '/style.css', //path to child (this) theme stylesheet
-        array( $parent_style ), //dependencies (child theme stylesheet is dependent on parent stylesheet)
-        wp_get_theme()->get('Version') //add child theme version
+        'gmuw-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        '',
+        time()
     );
+
+    //enqueue the child theme stylesheets
+
+    //RSS Blocks
+    wp_enqueue_style(
+        'gmuw-style-blockrss',
+        get_stylesheet_directory_uri() . '/css/block-rss.css',
+        '',
+        time()
+    );
+
+    // General style modifications to parent theme
+    wp_enqueue_style(
+        'gmuw-style-parentthememods',
+        get_stylesheet_directory_uri() . '/css/parent-theme-modifications.css',
+        '',
+        time()
+    );
+
+    // University links bar
+    wp_enqueue_style(
+        'gmuw-style-universitylinksbar',
+        get_stylesheet_directory_uri() . '/css/university-links-bar.css',
+        '',
+        time()
+    );
+
+    // University breadcrumbs bar
+    wp_enqueue_style(
+        'gmuw-style-universitybreadcrumbsbar',
+        get_stylesheet_directory_uri() . '/css/university-breadcrumbs-bar.css',
+        '',
+        time()
+    );
+
+    // Main navigation bar modifications to parent theme
+    wp_enqueue_style(
+        'gmuw-style-mainnavbar',
+        get_stylesheet_directory_uri() . '/css/main-navigation-bar.css',
+        '',
+        time()
+    );
+
+    // Widget areas
+    wp_enqueue_style(
+        'gmuw-style-widgetareas',
+        get_stylesheet_directory_uri() . '/css/widget-areas.css',
+        '',
+        time()
+    );
+
+    // Footer modifications to parent theme
+    wp_enqueue_style(
+        'gmuw-style-footer',
+        get_stylesheet_directory_uri() . '/css/footer.css',
+        '',
+        time()
+    );
+
+    // Collapse-O-Matic styles
+    wp_enqueue_style(
+        'gmuw-style-collapseomatic',
+        get_stylesheet_directory_uri() . '/css/collapse-o-matic.css',
+        '',
+        time()
+    );
+
+    // Breadcrumbs styles
+    wp_enqueue_style(
+        'gmuw-style-breadcrumbs',
+        get_stylesheet_directory_uri() . '/css/breadcrumbs.css',
+        '',
+        time()
+    );
+
+    // Sidebar styles
+    wp_enqueue_style(
+        'gmuw-style-sidebar',
+        get_stylesheet_directory_uri() . '/css/sidebar.css',
+        '',
+        time()
+    );
+
+    // Custom post type: person styles
+    wp_enqueue_style(
+        'gmuw-style-people',
+        get_stylesheet_directory_uri() . '/css/people.css',
+        '',
+        time()
+    );
+
+    // GMUJ Slider styles
+    wp_enqueue_style(
+        'gmuw-style-slideshow',
+        get_stylesheet_directory_uri() . '/css/slideshow.css',
+        '',
+        time()
+    );
+
+    // The Events Calendar styles
+    wp_enqueue_style(
+        'gmuw-style-eventscalendar',
+        get_stylesheet_directory_uri() . '/css/events-calendar.css',
+        '',
+        time()
+    );
+
+    // Details block
+    wp_enqueue_style(
+        'gmuw-style-details',
+        get_stylesheet_directory_uri() . '/css/details.css',
+        '',
+        time()
+    );
+
+    // Prominent Links menu
+    wp_enqueue_style(
+        'gmuw-style-prominentlinks',
+        get_stylesheet_directory_uri() . '/css/prominent-links.css',
+        '',
+        time()
+    );
+
 
 }
 
